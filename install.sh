88a0a6e5c3cd298238a0092d4dd7b1a579988bdd
seike460 <seike460@gmail.com>
2024-02-23 12:54:29 +0900

    install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 source ~/.bashrc
 pyenv --version
-pyenv install 3.12.0
-pyenv global 3.12.0
+pyenv install 3.12.2
+pyenv global 3.12.2
 python3.12 -m pip install --upgrade aws-sam-cli
 exec $SHELL -l
