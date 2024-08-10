d4f44a7673f957ec6f134c92f514f37aabad5f8d
seike460 <seike460@gmail.com>
2024-08-10 13:57:27 +0900

    愛媛ハンズオン用に調整

--- lambda/app.py
+++ lambda/app.py
@@ -1,24 +1,57 @@
 import boto3
 import json
 
+def invoke_bedrock_runtime_rag(prompt, max_tokens):
+    """bedrock-runtimeを呼び出してレスポンスを返す"""
+    client = boto3.client('bedrock-agent-runtime')
+    model_arn = "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0"
+    knowledgeBase_id = "XxxxxxxxxX" #メモ1を設定する
+    response = client.retrieve_and_generate(
+        input={
+            'text': prompt
+        },
+        retrieveAndGenerateConfiguration={
+        'type': 'KNOWLEDGE_BASE',
+        'knowledgeBaseConfiguration': {
+            'knowledgeBaseId': knowledgeBase_id,
+            'modelArn': model_arn
+        }
+    })
+    # 実行結果からテキストのみを変数に入れて、画面に出力する
+    generated_text = response['output']['text']
+    return generated_text
 
 def invoke_bedrock_runtime(prompt, max_tokens):
-    """bedrock-runtimeを呼び出してレスポンスを返す"""
-    client = boto3.client('bedrock-runtime')
-    body = {
-        'prompt': prompt,
-        'max_tokens_to_sample': max_tokens
-    }
-    response = client.invoke_model(
-        modelId='anthropic.claude-v2:1', body=json.dumps(body))
-    response_body = response['body'].read().decode('utf-8')
-    return json.loads(response_body)['completion']
-
+    bedrock = boto3.client("bedrock-runtime")
+    # リクエストボディを定義
+    body = json.dumps(
+        {
+            "anthropic_version": "bedrock-2023-05-31",
+            "max_tokens": max_tokens,
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+        }
+    )
+    # モデルを定義（Claude 3.5 Sonnet）
+    modelId = "anthropic.claude-3-5-sonnet-20240620-v1:0"
+    # HTTPヘッダーを定義
+    accept = "application/json"
+    contentType = "application/json"
+    # レスポンスを定義
+    response = bedrock.invoke_model(
+        body=body, modelId=modelId, accept=accept, contentType=contentType
+    )
+    response_body = json.loads(response.get("body").read().decode('utf-8'))
+    answer = response_body["content"][0]["text"]
+    return answer
 
 def create_prompt(user_message, assistant_message=''):
     """プロンプトを作成する"""
     return f"Human: {user_message}\nAssistant:{assistant_message}"
-
 
 def lambda_handler(event, context):
     # クエリパラメータを取得
@@ -27,12 +60,13 @@ def lambda_handler(event, context):
     param_value = ""
     max_tokens = 3000
     if query_params:
-        param_value = query_params.get('prompt', '北海道のジンギスカンが名物ですか？')
+        param_value = query_params.get('prompt', '愛媛の名物を教えてください')
         max_tokens = query_params.get('max_tokens', 3000)
     # プロンプト生成
     prompt = create_prompt(param_value)
     # Bedrock呼び出し
     completion_1 = invoke_bedrock_runtime(prompt, max_tokens)
+    #completion_1 = invoke_bedrock_runtime_rag(prompt, max_tokens)
     # 結果を返す
     return {
         'statusCode': 200,
